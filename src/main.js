b488d649ad36872fbbb4532b910968a2eff4bf3c
Дмитрий <selyxaa@yandex.ru>
2020-08-12 19:09:11 +0300

    правит еслинт

--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ import TopRatedFilmsExtraSection from "./view/top-rated-section.js";
 import MostCommentedFilmsExtraSection from "./view/most-commented-section.js";
 import PopupFilmDetails from "./view/popup-details.js";
 import NoFilmsMessage from "./view/no-film-message.js";
-import { generateFilm } from "./mock/film.js";
-import { generateFilters } from "./mock/filter.js";
-import { generateProfile } from "./mock/profile.js";
-import { render } from "./utils.js";
-import { MOVIES_COUNT } from "./mock/allmovies.js";
-import { RenderPosition } from "./utils.js";
-import { getRandomInteger } from "./utils.js";
+import {generateFilm} from "./mock/film.js";
+import {generateFilters} from "./mock/filter.js";
+import {generateProfile} from "./mock/profile.js";
+import {render} from "./utils.js";
+import {MOVIES_COUNT} from "./mock/allmovies.js";
+import {RenderPosition} from "./utils.js";
+import {getRandomInteger} from "./utils.js";
 
 const EXTRA_SECTIONS_FILMS_COUNT = 2;
 const NUMBER_OF_GENERATED_CARD = 22;
@@ -140,15 +140,10 @@ const createFilmsBoard = () => {
   const extraSections = filmsBoard.querySelectorAll(`.films-list--extra`);
 
   for (const section of extraSections) {
-    const extraSectionFilmsContainer = section.querySelector(
-      `.films-list__container`
-    );
+    const extraSectionFilmsContainer = section.querySelector(`.films-list__container`);
 
     for (let i = 0; i < EXTRA_SECTIONS_FILMS_COUNT; i++) {
-      renderFilm(
-        filmsCards[getRandomInteger(i, NUMBER_OF_GENERATED_CARD)],
-        extraSectionFilmsContainer
-      );
+      renderFilm(filmsCards[getRandomInteger(i, NUMBER_OF_GENERATED_CARD)], extraSectionFilmsContainer);
     }
   }
 };
