5cccfca79095fb29530452d29cb00a71f7efa8e7
Dmitriy Lobasov <36813348+IggyStooges@users.noreply.github.com>
2020-12-23 12:47:18 +0300

    Update render.js
    Финальные правки

--- src/utils/render.js
+++ src/utils/render.js
@@ -78,3 +78,18 @@ export const replace = (newChild, oldChild) => {
 
   parent.replaceChild(newChild, oldChild);
 };
+
+
+export const remove = (component) => {
+    if (component === null) {
+        return;
+    }
+
+    if (!(component instanceof AbstractView)) {
+        throw new Error(`Can remove only components`);
+    }
+
+    component.getElement().remove();
+    component.removeElement();
+};
+
